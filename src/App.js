ed879073e969c01acf2bba4b4b61f0ec080101a8
Ashutosh Tripathi <ashu500500@gmail.com>
2022-11-12 21:13:27 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import CharacterTable from './components/CharacterTable';
+import Search from './components/Search';
+import axios from 'axios';
+import React, {useEffect,useState} from 'react';
+import API from './api';
 
 function App() {
+  const[items,setItems] = useState([])
+  const[isLoading,setLoading] = useState(true)
+  const[query,setQuery] = useState((''))
+
+  useEffect(()=>{
+    const fetch = async() => {
+      if(query === ""){
+        const result = await axios.get(`http://gateway.marvel.com/v1/public/characters?ts=1&apikey=${API}`)
+        console.log(result.data.data.results)
+        setItems(result.data.data.results)
+        setLoading(false)
+      }else{
+        const result = await axios.get(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&ts=1&apikey=${API}`)
+        console.log(result.data.data.results)
+        setItems(result.data.data.results)
+        setLoading(false)
+      }
+    }
+    fetch()
+  },[query])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Search search = {(q)=>setQuery(q)}></Search>
+      <CharacterTable items={items} isLoading = {isLoading} />
     </div>
   );
 }
